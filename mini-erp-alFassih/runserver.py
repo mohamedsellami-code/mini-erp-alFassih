948232dba1ad72785cdbdcb32ac3f2ca3dcbaaf0
MOHAMED SELLAMI <MOHAMED SELLAMI@SELLAMI-MOHAMED>
2025-06-07 16:51:37 +0100

    Ajoutez des fichiers projet.

---
+++ mini-erp-alFassih/runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the mini_erp_alFassih application using a development server.
+"""
+
+from os import environ
+from mini_erp_alFassih import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
